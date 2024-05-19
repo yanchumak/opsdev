6e6f69b727ea8598f5b95af650ea929b9290ce68
Yan <yanc@ext.inditex.com>
2024-05-19 21:36:35 +0200

    2

--- iaas/static-website/main.tf
+++ iaas/static-website/main.tf
@@ -3,11 +3,16 @@ provider "aws" {
     region = var.region
 }
 
+resource "random_string" "random" {
+  length = 6
+  special = false
+  upper = false
+} 
+
 resource "aws_s3_bucket" "bucket" {
-  bucket = "website"
+  bucket = "website-${random_string.random.result}"
   force_destroy = true
 }
-
 resource "aws_s3_bucket_website_configuration" "website" {
   bucket = aws_s3_bucket.bucket.id
   index_document {
